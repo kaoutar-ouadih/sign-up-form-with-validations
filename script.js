575f1ee71c6af8eace0d57e9465ff9011bcb96f6
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-04-03 22:38:17 +0000

    adjust placeholders in error case

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function validateFirstName(){
         nameErroIcon.innerHTML = "<svg width='24' height='24' xmlns='http://www.w3.org/2000/svg'><g fill='none' fill-rule='evenodd'><circle fill='#FF7979' cx='12' cy='12' r='12'/><rect fill='#FFF' x='11' y='6' width='2' height='9' rx='1'/><rect fill='#FFF' x='11' y='17' width='2' height='2' rx='1'/></g></svg>"
         firstName.style.border = '2px solid hsl(0, 100%, 74%)';
         firstName.style.borderRadius = '3px';
+        firstName.placeholder = '';
         return false;
     }
     firstNameError.innerHTML = "";
@@ -35,6 +36,7 @@ function validateLastName(){
         lastNameErrorIcon.innerHTML = "<svg width='24' height='24' xmlns='http://www.w3.org/2000/svg'><g fill='none' fill-rule='evenodd'><circle fill='#FF7979' cx='12' cy='12' r='12'/><rect fill='#FFF' x='11' y='6' width='2' height='9' rx='1'/><rect fill='#FFF' x='11' y='17' width='2' height='2' rx='1'/></g></svg>"
         lastName.style.border = '2px solid hsl(0, 100%, 74%)';
         lastName.style.borderRadius = '3px';
+        lastName.placeholder = '';
         return false;
     }
     lastNameError.innerHTML = "";
@@ -48,6 +50,8 @@ function validateEmail(){
         emailErrorIcon.innerHTML = "<svg width='24' height='24' xmlns='http://www.w3.org/2000/svg'><g fill='none' fill-rule='evenodd'><circle fill='#FF7979' cx='12' cy='12' r='12'/><rect fill='#FFF' x='11' y='6' width='2' height='9' rx='1'/><rect fill='#FFF' x='11' y='17' width='2' height='2' rx='1'/></g></svg>"
         email.style.border = '2px solid hsl(0, 100%, 74%)';
         email.style.borderRadius = '3px';
+        email.placeholder = 'email@example/com';
+        email.classList.add('red-placeholder');
         return false;
     }
     emailError.innerHTML = "";
@@ -61,6 +65,7 @@ function validatePassword(){
         passwordErrorIcon.innerHTML = "<svg width='24' height='24' xmlns='http://www.w3.org/2000/svg'><g fill='none' fill-rule='evenodd'><circle fill='#FF7979' cx='12' cy='12' r='12'/><rect fill='#FFF' x='11' y='6' width='2' height='9' rx='1'/><rect fill='#FFF' x='11' y='17' width='2' height='2' rx='1'/></g></svg>"
         password.style.border = '2px solid hsl(0, 100%, 74%)';
         password.style.borderRadius = '3px';
+        password.placeholder = '';
         return false;
     }
     passwordError.innerHTML = "";
